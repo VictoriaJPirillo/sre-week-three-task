272b3ecfefb5fd8b67ff17492efe0be1a3697a2c
Victoria J Pirillo <126045232+VictoriaJPirillo@users.noreply.github.com>
2024-04-23 21:22:26 +0000

    On branch main  Changes to be committed: 	new file:   nohup.out 	modified:   swype-deployment.yml 	modified:   watcher.sh Week 3 project - midway progress

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,23 @@
-#TODO
+# Big Red Button - bash script to monitor Kubernetes deployment
+
+# Written by Victoria J Pirillo
+
+# 1. Define Variables: Set the namespace, deployment name, and maximum number of restarts allowed before scaling down the deployment.
+namespace="sre"
+DEPLOYMENT_NAME
+MAX_RESTARTS_ALLOWED=5
+
+# 2. Start a Loop: Begin an infinite loop that will continue until explicitly broken.
+
+
+# 3. Check Pod Restarts: Within the loop, use the kubectl get pods command to retrieve the number of restarts of the pod associated with the specified deployment in the specified namespace.
+
+# 4. Display Restart Count: Print the current number of restarts to the console.
+
+# 5. Check Restart Limit: Compare the current number of restarts with the maximum allowed number of restarts.
+
+# 6. Scale Down if Necessary: If the number of restarts is greater than the maximum allowed, print a message to the console, scale down the deployment to zero replicas using the kubectl scale command, and break the loop.
+
+# 7. Pause: If the number of restarts is not greater than the maximum allowed, pause the script for 60 seconds before the next check.
+
+# 8. Repeat: After the pause, the script goes back to step 3. This process repeats indefinitely until the number of restarts exceeds the maximum allowed, at which point the deployment is scaled down and the loop is broken.
