7cf226eaf353e39f08434fac9943a3edb6057ee6
Victoria J Pirillo <126045232+VictoriaJPirillo@users.noreply.github.com>
2024-04-26 20:52:33 +0000

    modified:   watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -10,27 +10,22 @@ DEPLOYMENT="swype-app"
 # Maximum number of restarts before scaling down
 MAX_RESTARTS=4
 
-# 2. Start a Loop: Begin an infinite loop that will continue until explicitly broken.
+# 2. Start infinite loop:
 while true; do 
- # Get the number of restarts of the pod
- RESTARTS=$(kubectl get pods -n ${NAMESPACE} -1 app=${DEPLOYMENT} -o JSONPATH="{.items[0].status.containerStatuses[0].restartCount}")
 
+ # Check pod restarts: Get the number of restarts of the pod
+ RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+ # Display the restart count:
   echo "Current number of restarts: ${RESTARTS}"
 
-    # If the number of restarts is greater than the maximum allowed, scale down the deployment
+    # Scale down deployment if max number restarts exceeds the maximum allowed
   if (( RESTARTS > MAX_RESTARTS )); then
     echo "Maximum number of restarts exceeded. Scaling down the deployment..."
     kubectl scale --replicas=0 deployment/${DEPLOYMENT} -n ${NAMESPACE}
     break
   fi
-# 3. Check Pod Restarts: Within the loop, use the kubectl get pods command to retrieve the number of restarts of the pod associated with the specified deployment in the specified namespace.
 
-# 4. Display Restart Count: Print the current number of restarts to the console.
-
-# 5. Check Restart Limit: Compare the current number of restarts with the maximum allowed number of restarts.
-
-# 6. Scale Down if Necessary: If the number of restarts is greater than the maximum allowed, print a message to the console, scale down the deployment to zero replicas using the kubectl scale command, and break the loop.
-
-# 7. Pause: If the number of restarts is not greater than the maximum allowed, pause the script for 60 seconds before the next check.
-
-# 8. Repeat: After the pause, the script goes back to step 3. This process repeats indefinitely until the number of restarts exceeds the maximum allowed, at which point the deployment is scaled down and the loop is broken.
+  # Wait before doing next check
+  sleep 60
+done
